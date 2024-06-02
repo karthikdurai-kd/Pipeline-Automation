982c3341be1c3e6d24a1659c3e8a57f3f57d2456
Karthikdurai1994 <146772280+KarthikDurai98@users.noreply.github.com>
2024-06-02 19:01:48 -0400

    Project Initialization

--- lib/aws-ci-cd-project-stack.ts
+++ lib/aws-ci-cd-project-stack.ts
@@ -1,16 +1,9 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { CodePipeline } from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
 
 export class AwsCiCdProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCiCdProjectQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
