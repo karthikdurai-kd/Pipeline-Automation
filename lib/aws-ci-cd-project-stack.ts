c8b8c20357701c09e54ce2e04d06b5f8e94e7ffd
Karthikdurai1994 <146772280+KarthikDurai98@users.noreply.github.com>
2024-06-02 19:24:34 -0400

    Added Output directory for building pipeline

--- lib/aws-ci-cd-project-stack.ts
+++ lib/aws-ci-cd-project-stack.ts
@@ -1,9 +1,26 @@
 import * as cdk from "aws-cdk-lib";
-import { CodePipeline } from "aws-cdk-lib/pipelines";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
 export class AwsCiCdProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create AWS CodePipeline stack
+    new CodePipeline(this, "PipelineAutomation", {
+      pipelineName: "PipelineAutomation",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub(
+          "karthikdurai-kd/Pipeline-Automation",
+          "main"
+        ),
+        commands: ["cd AWS-CI-CD-PROJECT", "npm ci", "npx cdk synth"],
+        primaryOutputDirectory: "cdk.out",
+      }),
+    });
   }
 }
