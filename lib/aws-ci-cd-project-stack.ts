e25a742b40d78c4bfe79d8e179e9633157cd1dfa
Karthikdurai1994 <146772280+KarthikDurai98@users.noreply.github.com>
2024-06-03 12:30:47 -0400

    Created Pipeline stage for test

--- lib/aws-ci-cd-project-stack.ts
+++ lib/aws-ci-cd-project-stack.ts
@@ -5,13 +5,14 @@ import {
   ShellStep,
 } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
+import { PipelineStage } from "./PipelineStage";
 
 export class AwsCiCdProjectStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // Create AWS CodePipeline stack
-    new CodePipeline(this, "PipelineAutomation", {
+    const pipeline = new CodePipeline(this, "PipelineAutomation", {
       pipelineName: "PipelineAutomation",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub(
@@ -21,5 +22,12 @@ export class AwsCiCdProjectStack extends cdk.Stack {
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
     });
+
+    // Creating test stage
+    const testStage = pipeline.addStage(
+      new PipelineStage(this, "PipelineTestStage", {
+        stageName: "test",
+      })
+    );
   }
 }
