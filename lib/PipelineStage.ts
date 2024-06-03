54c6bc94aa927da15dfb602a766ff4313ef03a65
Karthikdurai1994 <146772280+KarthikDurai98@users.noreply.github.com>
2024-06-03 16:52:11 -0400

    Created a Lambda function

--- lib/PipelineStage.ts
+++ lib/PipelineStage.ts
@@ -6,6 +6,7 @@ export class PipelineStage extends Stage {
   constructor(scope: Construct, id: string, props: StageProps) {
     super(scope, id, props);
 
+    // What we are doing here is we are creating a LambdaStack using AWS CodePipeline. That is we write the code and push it to github and from there it is built by AWS CodePipeline. Previously we will manually do "CDK Deploy" but now once we push the code to github, it built by AWS CodePipeline
     new LambdaStack(this, "LambdaStack", {
       stageName: props.stageName,
     });
